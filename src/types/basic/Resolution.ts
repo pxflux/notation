ba8712f67446287a444605cf5f870b815b04ae95
maxim <tyminko@gmail.com>
2020-02-11 22:13:51 +0100

    update Resolution (add 'from' method)

--- src/types/basic/Resolution.ts
+++ src/types/basic/Resolution.ts
@@ -1,9 +1,14 @@
 import closestStandardForResolution from '../../utilites/standard-resolutions'
+import { castToType } from '../../utilites/cast'
 
 export class Resolution {
 
   w = 0
   h = 0
+
+  static from (object: Resolution): Resolution {
+    return castToType(object, Resolution)
+  }
 
   toString () {
     const std = closestStandardForResolution(this)
